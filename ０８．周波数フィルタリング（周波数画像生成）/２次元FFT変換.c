1ff57813d637d9ebc05e0d89b3bc066c9248a6b4
rikuriku <94785419+Rikkuriku@users.noreply.github.com>
2024-03-03 00:37:53 +0900

    Create ２次元FFT変換.c

---
+++ ０８．周波数フィルタリング（周波数画像生成）/２次元FFT変換.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <math.h>
+#include "pgmlib.h"
+#include "fft.h" // data[],jdata[],num_of_dataを用います
+
+void make_original_data(int n) //教科書p.73
+{
+  int i, j;
+  
+  if (width[n]!=height[n]) {
+    printf("縦と横の画素数が異なっています。\n");
+    exit(1);
+  }
+  printf("\n読み込んだ画像を変換元データに直します。");
+  
+  num_of_data = width[n];
+  for (j=0; j<num_of_data; j++) {
+    for (i=0; i<num_of_data; i++) {
+      data[i][j]=(double)image[n][i][j];
+      jdata[i][j]=0.0; // 虚部
+    }
+  }
+}
+
+void make_output_image(int n)
+{
+  int x, y;
+  double val;
+  
+  printf("\n逆FFT変換後の画像を作成します。\n");
+  width[n]=height[n]=num_of_data;
+  double max=0;
+  for (y=0; y<height[n]; y++) {
+    for (x=0; x<width[n]; x++) {
+      //val=sqrt(data[x][y]*data[x][y]+jdata[x][y]*jdata[x][y]);
+      val=data[x][y];
+      if (max<fabs(val)) max=val;
+    }
+  }
+  printf("最大値：%f\n", max);
+  for (y=0; y<height[n]; y++) {
+    for (x=0; x<width[n]; x++) {
+      val=(sqrt(data[x][y]*data[x][y]+jdata[x][y]*jdata[x][y]));
+      if (val<0) val=0;
+      if (val>MAX_BRIGHTNESS) val=MAX_BRIGHTNESS;
+      image[n][x][y]=(unsigned char)val;
+    }
+  }
+}
+
+//Sin_wave
+int main()
+{
+  //処理する元画像の読込み
+  load_image(0, "sinwave_N_64.pgm");
+
+  make_original_data(0); 
+  FFT2(1); // ２次元空間画像を２次元FFT変換し周波数領域へ
+  make_output_image(1); 
+
+  //結果画像を保存
+  save_image(1, "fft_result.pgm");
+}
